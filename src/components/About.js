5ccdd0a3283db1303c3bb7b0e9067fb3cd17fc07
viyan-najmaldin <najmadinviyan@gmail.com>
2022-10-29 19:39:54 +0300

    Done assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,20 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      {" "}
+      <h2>About Me</h2>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia harum
+        in laboriosam ullam quis perspiciatis praesentium eum! Enim
+        necessitatibus reprehenderit obcaecati libero officia aliquid et
+        dolores, possimus id iste culpa.{" "}
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
